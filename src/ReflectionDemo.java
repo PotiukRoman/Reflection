08c4ed5d17c6a27cbe503d2a1b1403620ec82a12
PotiukRoman <roman.potyuk@openexc.com>
2022-07-02 18:00:58 +0300

    initial commit

---
+++ src/ReflectionDemo.java
@@ -0,0 +1,52 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionDemo {
+
+    public static void main(String args[]) throws Exception
+    {
+        Test testObj = new Test();
+        Test testObj1 = new Test();
+
+        Class classObject = testObj.getClass();
+
+        System.out.println("The name of class is " + classObject.getName());
+
+        Constructor constructor = classObject.getConstructor();
+
+        System.out.println("The name of constructor is " + constructor.getName());
+
+        System.out.println("The public methods of class are : ");
+
+        Method[] methods = classObject.getMethods();
+
+        for (Method method : methods)
+            System.out.println(method.getName());
+
+        Method methodcall1 = classObject.getDeclaredMethod("method1");
+
+        methodcall1.invoke(testObj);
+        methodcall1.invoke(testObj1);
+
+        Field field = classObject.getDeclaredField("s");
+
+        field.setAccessible(true);
+
+        field.set(testObj, "testObj TestStringChanged");
+        field.set(testObj1, "test1 TestStringChanged");
+
+        methodcall1.invoke(testObj);
+        methodcall1.invoke(testObj1);
+
+        Method methodcall2 = classObject.getDeclaredMethod("method2", int.class);
+
+        methodcall2.invoke(testObj, 25);
+
+        Method methodcall3 = classObject.getDeclaredMethod("method3");
+
+        methodcall3.setAccessible(true);
+
+        methodcall3.invoke(testObj);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+
+    private String s;
+
+    public Test() {
+        s = "TestString";
+    }
+
+    public void method1()
+    {
+        System.out.println("The string is " + s);
+    }
+
+    public void method2(int n)
+    {
+        System.out.println("The number is " + n);
+    }
+
+    private void method3()
+    {
+        System.out.println("Private method invoked");
+    }
+}
